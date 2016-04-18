1c9547c8abcf5c7eb62dd1a91243d0f22cf634ec
BrendanMurray <thebmurr@gmail.com>
2016-04-18 12:26:23 -0400

    added testSubmitQuestion and testReviewQuestion

--- AdminTestCases.java
+++ AdminTestCases.java
@@ -83,4 +83,40 @@ public class AdminTestCases {
 			fail();
 		}
 	}
+	
+	@Test
+	public void testSubmitQuestion(){
+		driver.get(baseUrl);
+		try{
+			driver.findElement(By.id("Questions")).click();
+			driver.findElement(By.id("submitNew")).click();
+			driver.findElement(By.id("questionText")).clear();
+			driver.findElement(By.id("questionText")).sendKeys("What is your favorite color?");
+			driver.findElement(By.id("answer1")).clear();
+			driver.findElement(By.id("answer1")).sendKeys("this is red");
+			driver.findElement(By.id("answer2")).clear();
+			driver.findElement(By.id("answer2")).sendKeys("this is blue");
+			driver.findElement(By.id("answer3")).clear();
+			driver.findElement(By.id("answer3")).sendKeys("this is green");
+			driver.findElement(By.id("answer4")).clear();
+			driver.findElement(By.id("answer4")).sendKeys("this is yellow");
+			driver.findElement(By.id("select2")).click();
+			if (!driver.getPageSource().contains("Question Submitted Successfully!")) fail();
+		} catch (NoSuchElementException nseex) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testReviewQuestion(){
+		driver.get(baseUrl);
+		try{
+			driver.findElement(By.id("Questions")).click();
+			driver.findElement(By.id("reviewNew")).click();
+			if (!driver.getPageSource().contains("What is your favorite color?")) fail();
+		} catch (NoSuchElementException nseex) {
+			fail();
+		}
+	}
+	
 }
